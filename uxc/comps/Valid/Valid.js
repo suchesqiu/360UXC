3a86bbda6c3835ce4b1e7fd2dbf8b81613b115f2
suchesqiu <suches@btbtd.org>
2013-05-22 13:51:54 +0800

    添加自动生成UXC组件库文档功能

--- uxc/comps/Valid/Valid.js
+++ uxc/comps/Valid/Valid.js
@@ -4,12 +4,12 @@
      * 表单验证类
      * @alias UXC.Valid
      * @class
-     * @public
      * @global
      *
-     * @desc提供两个静态方法供外部调用
+     * @classdesc 提供两个静态方法供外部调用
      *      分别为 
-     *          Valid.check      return  bool     验证一个表单项, 如文本框/选择框     
+     *          外部访问请使用 UXC.Valid<br />
+     *          Valid.check      return  bool     验证一个表单项, 如文本框/选择框<br/>     
      *          Valid.checkAll   return  bool     验证整个表单
      *
      * @author          qiushaowei   2013-05-22
@@ -20,7 +20,6 @@
     /**
      * 验证一个表单项, 如 文本框, 下拉框, 复选框, 单选框, 文本域, 隐藏域
      * @method
-     * @public
      * @memberof Valid
      *
      * @param      {elements}    _fmItem -   需要验证规则正确与否的表单项
@@ -37,7 +36,6 @@
      /**
      * 验证整个表单是否符合规则
      * @method
-     * @public
      * @memberof Valid
      *
      * @param      {elements}   _fm -       需要验证规则正确与否的表单项
