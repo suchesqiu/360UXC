95e91ba3a35959cdb1c731de188b6b11ad3b1eac
suchesqiu <x@btbtd.org>
2013-05-31 22:54:04 +0800

    发布UXC资源控制器及Valid组件

--- uxc/UXC.js
+++ uxc/UXC.js
@@ -1,108 +1,137 @@
 (function( $ ){
     /**
-     * UXC jq 组件库 资源调用控制类
+     * UXC jq 组件库 资源调用控制类<br />
+     * 这是一个单例模式, 全局访问使用 UXC 或 window.UXC
+     * @namespace window
+     * @class UXC
+     * @static
+     * @requires    jQuery
+     * @example 
+     *      UXC.import( 组件名[,组件名] );
      *
-     * 调用组件资源请使用  UXC.import( 组件名[,组件名] );
-     *
-     * @author qiushaowei   2013-05-21
+     * @author  qiushaowei   <suches@btbtd.org> | {@link http://uxc.360.cn|360 UXC-FE Team}
+     * @date    2013-05-22
      */
-    function UXC(){
-        var _p = this;
-
+    window.UXC = 
+    {
         /**
          * UXC组件库所在路径
+         * @property    PATH
+         * @type {string}
          */
-        _p.PATH = '/js/uxc/comps/';
+        PATH: '/js/uxc/comps/'
         /**
          * 是否显示调试信息
+         * @property    debug
+         * @type {bool}
          */
-        _p.debug;
-        /**
-         * 自动识别组件库所在路径
-         */
-        _p._getPath();
-    }
-
-    UXC.prototype = {
+        , debug: false
        /**
         * 导入UXC组件
-        * @param    _names      string      模块名
-        *                                   或者模块下面的某个js文件(test/test1.js, 路径前面不带"/"将视为test模块下的test1.js)
-        *                                   或者一个绝对路径的js文件, 路径前面带 "/"
+        * @method   import
+        * @param    {string}    _names -            模块名
+        *                                           或者模块下面的某个js文件(test/test1.js, 路径前面不带"/"将视为test模块下的test1.js)
+        *                                           或者一个绝对路径的js文件, 路径前面带 "/"
         *
-        * @param    _basePath   string      指定要导入资源所在的主目录, 这个主要应用于 nginx 路径输出
-        * @param    _nginxStyle bool        指定是否需要使用 nginx 路径输出脚本资源
+        * @param    {string}    _basePath -         指定要导入资源所在的主目录, 这个主要应用于 nginx 路径输出
+        * @param    {bool}      _nginxStyle -       指定是否需要使用 nginx 路径输出脚本资源
         *
         * @example
-                UXC.import( 'Test/Test1.js' );
-                UXC.import( 'Test/Test1.js' );
-                UXC.import( '/js/Test/Test1.js' );
+                UXC.import( 'SomeClass' );                              //导入类 SomeClass
+                UXC.import( 'SomeClass, AnotherClass' );                //导入类 SomeClass, AnotherClass
+                //
+                ///  导入类 SomeClass, SomeClass目录下的file1.js, 
+                ///  AnotherClass, AnotherClass 下的file2.js
+                //
+                UXC.import( 'SomeClass, SomeClass/file1.js, AnotherClass/file2.js' );   
+                UXC.import( '/js/Test/Test1.js' );                      //导入文件  /js/Test/Test1.js, 如果起始处为 "/", 将视为文件的绝对路径
+                //
+                /// nginx style 的文件加载方式, 如 /js/??file1.js,file2.js,file3.js
+                //
                 UXC.import( 'Test1.js, Test2.js ', '/js/??', true );
+                //
+                /// 导入 URL 资源
+                //
+                UXC.import( 'http://test.com/file1.js', 'https://another.com/file2.js' );
         */
-        import: function( _names, _basePath, _nginxStyle ){
-                    if( ! _names ) return;
-                    var _p = this;
+        , import: 
+            function( _names, _basePath, _nginxStyle ){
+                if( ! _names ) return;
+                var _p = this, _urlRe = /\:\/\//;
 
-                    var _paths = [];
+                var _paths = [];
 
-                    $.each( _names.split(/[\s]*?,[\s]*/), function( _ix, _val ){
+                $.each( _names.split(/[\s]*?,[\s]*/), function( _ix, _val ){
 
-                        var _isCustomPath = /\//.test( _val ) || !!_basePath;
+                    var _isCustomPath = _urlRe.test(_val) || /\//.test( _val ) || !!_basePath;
 
-                        if( !/\.js$/i.test( _val ) & !_basePath )  _val =  [ _val, '/', _val, '.js' ].join('');
+                    if( !/\.js$/i.test( _val ) & !_basePath )  _val =  [ _val, '/', _val, '.js' ].join('');
 
-                        if( _isCustomPath ){
-                            if( _basePath && !_nginxStyle ) _val = _basePath + _val;
-                            else if( !/[\/\\]/.test( _val.slice( 0, 1 ) ) && !_nginxStyle ) _val = _p.PATH + _val;
-                        }else{
-                            _val = _p.PATH + _val;
-                        }
-                        /**
-                         * 去除多余的 正叙扛或反叙扛
-                         */
-                        _val = _val.replace( /(\\)\1|(\/)\2/g, '$1' ); 
-
-                        if( !_nginxStyle ){
-                            _paths.push( '<script src="'+_val+'"><\/script>' );
-                        }else{
-                            _paths.push( _val );
-                        }
-
-                        _p.log( _val );
-                    });
-
-                    if( _nginxStyle ){
-                        _basePath += _paths.join();
-                        document.write( '<script src="'+_basePath+'"><\/script>' );
+                    if( _isCustomPath ){
+                        if( _urlRe.test( _val ) ){} 
+                        else if( _basePath && !_nginxStyle ) _val = _basePath + _val;
+                        else if( !/[\/\\]/.test( _val.slice( 0, 1 ) ) && !_nginxStyle ) _val = _p.PATH + _val;
                     }else{
-                        document.write( _paths.join('') );
+                        _val = _p.PATH + _val;
+                    }
+                    /**
+                     * 去除多余的 正叙扛或反叙扛
+                     * @private
+                     */
+                    !_urlRe.test( _val ) && ( _val = _val.replace( /(\\)\1|(\/)\2/g, '$1' ) ); 
+
+                    if( !_nginxStyle ){
+                        _paths.push( '<script src="'+_val+'"><\/script>' );
+                    }else{
+                        _paths.push( _val );
                     }
 
-                    _p.log( _paths );
+                    _p.log( _val );
+                });
+
+                if( _nginxStyle ){
+                    _basePath += _paths.join();
+                    document.write( '<script src="'+_basePath+'"><\/script>' );
+                }else{
+                    document.write( _paths.join('') );
                 }
+
+                _p.log( _paths );
+            },
         /**
          * 获取组件库所在路径
+         * @method
+         * @private
+         * @return     {string}    组件库所在路径(带comps)
          */
-        , _getPath: function(){
-                      var _sc = $('script').last(), _path = _sc.attr('src').replace( /UXC\.js[^\w]*/i, '' );
-                      if( _path ) this.PATH = _path + 'comps/';
-                  }
+        _getPath: 
+            function(){
+                var _sc = $('script').last(), _path = _sc.attr('src');
+                if( /\//.test( _path ) ){ _path = _path.split('/'); _path.pop(); _path = _path.join('/') + '/'; }
+                else if( /\\/.test( path ) ){ _path = _path.split('\\'); _path.pop(); _path = _path.join('\\') + '/'; }
+                this.PATH = _path + 'comps/';
+            },
 
        /**
         * 输出调试信息, 可通过 UXC.debug 指定是否显示调试信息
+        * @param    {[string[,string]]}  任意参数任意长度的字符串内容
+        * @method log
         */
-       , log: function(){
-            if( !this.debug ) return;
-            console.log( [].slice.apply( arguments ).join(', ') );
-       }
+       log: 
+           function(){
+                if( !this.debug ) return;
+                console.log( [].slice.apply( arguments ).join(' ') );
+            }
     };
-
     /**
      * 如果 console 不可用, 则生成一个模拟的 console 对象
      */
     if( !window.console ) window.console = { log:function(){
-        window.status = [].slice.apply( arguments ).join(', ');
+        window.status = [].slice.apply( arguments ).join(' ');
     }};
+    /**
+     * 自动识别组件库所在路径
+     */
+    UXC._getPath();
 
-    window.UXC = new UXC();
 }(jQuery))
