33a1a07b767938fcf9527b3a69069e374ce7eb7a
suchesqiu <x@btbtd.org>
2013-06-02 22:33:52 +0800

    tmp

--- uxc/comps/Calendar/Calendar.js
+++ uxc/comps/Calendar/Calendar.js
@@ -2,18 +2,25 @@
     !window.UXC && (window.UXC = { log:function(){} });
 
     UXC.Calendar = window.Calendar = Calendar;
-    
+
+    function Calendar( _selector )
+    {
+        this._model = new Model( _selector ).init();
+        this._view = new View( this.model ).init();
+    }
+     
     Calendar.pickDate =
         function( _selector ){
             return new Calendar( _selector ).init();
         };
 
-    function Calendar( _selector )
-    {
-        this.model = new Model( _selector ).init();
-        this.view = new View( this.model ).init();
-    }
-    
+    $(document).ready( function($evt){
+        $('input[type=text]').each( function($evt){
+            var _p = $(this);
+            if( $.trim( _p.attr('datatype') || '').toLowerCase() == 'date' ) Calendar.init( _p );
+        });
+    });
+   
     Calendar.prototype =
     {
         init:
