fee9baaa6bb44742fe40dc33d84bb2baa30e785c
suchesqiu <x@btbtd.org>
2013-06-10 18:59:26 +0800

    添加合并jquery.js, UXC.js 脚本

---
+++ uxc/node_merge_lib.js
@@ -0,0 +1,11 @@
+var fs = require('fs-extra');
+var compressor = require('node-minify');
+
+var dir = __dirname + '/';
+
+var tmp = [];
+    tmp.push( fs.readFileSync( dir + 'jquery.js', 'utf8') );
+    tmp.push( fs.readFileSync(  dir + 'UXC.js', 'utf8') );
+
+
+fs.writeFileSync( dir + 'lib.full.js', tmp.join('\n') );
