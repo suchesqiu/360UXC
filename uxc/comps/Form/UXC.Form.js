75d0e7047b1dcd5f677e6668e562c05f43ef4c54
suchesqiu <x@btbtd.org>
2013-06-11 17:56:49 +0800

    完成Form.initCheckAll

---
+++ uxc/comps/Form/UXC.Form.js
@@ -0,0 +1,17 @@
+;(function($){
+    !window.UXC && (window.UXC = { log:function(){} });
+    /**
+     * 表单常用功能类 UXC.Form
+     * <p><b>requires</b>: <a href='window.jQuery.html'>jQuery</a></p>
+     * <p><a href='https://github.com/suchesqiu/360UXC.git' target='_blank'>UXC Project Site</a>
+     * | <a href='http://uxc.btbtd.org/docs/uxc_docs/classes/UXC.Form.html' target='_blank'>API docs</a>
+     * | <a href='http://uxc.btbtd.org/uxc/comps/Form/_demo' target='_blank'>demo link</a></p>
+     * @namespace UXC
+     * @class Form
+     * @static
+     * @version dev 0.1
+     * @author  qiushaowei   <suches@btbtd.org> | 360 UXC-FE Team
+     * @date    2013-06-11
+     */
+    window.UXCForm = UXC.Form = {};
+}(jQuery));
