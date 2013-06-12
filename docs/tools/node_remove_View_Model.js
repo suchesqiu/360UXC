b664393075c60ae078f5d9680862ea28f721e169
suchesqiu <x@btbtd.org>
2013-06-12 22:04:46 +0800

    发布UXC.Form.initCheckAll, UXC.Form.initAutoSelect

---
+++ docs/tools/node_remove_View_Model.js
@@ -0,0 +1,39 @@
+var fs = require('fs-extra');
+var dir = '../uxc_docs/classes';
+
+removeIgnoreItem( '../uxc_docs/index.html' );
+
+var dirs = fs.readdirSync( dir );
+
+for( var i = 0, j = dirs.length; i < j; i++ ){
+    var path = dir + '/' + dirs[i];
+
+    var fstat = fs.statSync( path );
+
+    if( fstat.isDirectory() ){
+        /*console.log('directory');*/
+    }else if( fstat.isFile ){
+        /*console.log('file');*/
+    }
+
+    if( fstat.isDirectory() ){
+    }else if( fstat.isFile() ){
+        removeIgnoreItem( path );
+    }
+
+}
+
+function removeIgnoreItem( _path ){
+    var fstr = fs.readFileSync( _path, 'utf8' );
+    var ar = fstr.split( /[\r\n]+/ );
+    var r = [];
+
+    for( var i = 0, j = ar.length; i < j; i++ ){
+        if( /^[\s]*$/.test( ar[i] ) ) continue;
+        if( /\.Model|\.View/.test( ar[i] ) ) continue;
+        r.push( ar[i] );
+    }
+
+    fs.writeFileSync( _path, r.join('\r\n') );
+}
+
--- docs/uxc_docs/api.js
+++ docs/uxc_docs/api.js
@@ -1,1 +1,21 @@
-YUI.add("yuidoc-meta",function(a){a.YUIDoc={meta:{classes:["UXC.Calendar","UXC.Dialog","UXC.Dialog.alert","UXC.Dialog.confirm","UXC.Panel","UXC.Panel.Model","UXC.Valid","UXC.View","UXC.alert","UXC.confirm","window.UXC","window.jQuery"],modules:[],allModules:[]}}});
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "UXC.Calendar",
+        "UXC.Dialog",
+        "UXC.Dialog.alert",
+        "UXC.Dialog.confirm",
+        "UXC.Form",
+        "UXC.Panel",
+        "UXC.Panel.Model",
+        "UXC.Panel.View",
+        "UXC.Valid",
+        "UXC.alert",
+        "UXC.confirm",
+        "window.UXC",
+        "window.jQuery"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
