6217cd6e199b9511377c2be3a79cd13a614ed725
suchesqiu <x@btbtd.org>
2013-07-03 16:11:24 +0800

    调整目录结构

--- merge.uxc.init.sh
+++ merge.uxc.init.sh
@@ -1,15 +1,15 @@
 dir=$PWD; 
 
-sh -c "cd $dir/docs/tools && sh generate_yuidoc_for_uxc.sh";
+sh -c "cd $dir/tools && sh generate_yuidoc_for_uxc.sh";
 
 sleep 3s
 
 echo "sleep done";
 
-sh -c "cd $dir/uxc && node nodejs_merge.js";
-sh -c "cd $dir/uxc/comps/Calendar && node nodejs_merge.js";
-sh -c "cd $dir/uxc/comps/Panel && node nodejs_merge.js";
-sh -c "cd $dir/uxc/comps/Form && node nodejs_merge.js";
-sh -c "cd $dir/uxc/comps/LunarCalendar && node nodejs_merge.js";
+sh -c "node nodejs_merge.js";
+sh -c "cd $dir/comps/Calendar && node nodejs_merge.js";
+sh -c "cd $dir/comps/Panel && node nodejs_merge.js";
+sh -c "cd $dir/comps/Form && node nodejs_merge.js";
+sh -c "cd $dir/comps/LunarCalendar && node nodejs_merge.js";
 
 echo $dir;
